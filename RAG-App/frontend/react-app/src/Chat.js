e2489ca125af24d16a30da03bfb53fbe7fb81035
ahh28964 <ahh28964@gmail.com>
2026-03-02 19:27:55 -0600

    Frotend updates: login screen with hardcoded credentials and working login and logout buttons, start of admin, user, and chat screens complete

---
+++ RAG-App/frontend/react-app/src/Chat.js
@@ -0,0 +1,9 @@
+import React, {useState} from "react";
+import './Chat.css';
+
+// this will be used for making the AI chat box
+function Chat() {
+    return "Welcome to the Chat";
+}
+
+export default Chat;
